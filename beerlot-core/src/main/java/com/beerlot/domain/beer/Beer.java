c9fe461e2c5d4794b131d9000aef3645a872a42c
han-gyeong <hangyeong@outlook.com>
2025-05-10 00:54:17 +0900

    Fix: Simplify review rating calculation

--- beerlot-core/src/main/java/com/beerlot/domain/beer/Beer.java
+++ beerlot-core/src/main/java/com/beerlot/domain/beer/Beer.java
@@ -86,7 +86,7 @@ public class Beer extends CreateAndUpdateDateTime {
     }
 
     public void calculateRate(float rate) {
-        this.rate = rate > 0 ? (this.rate * (reviewCount - 1) + rate) / Math.min(reviewCount, 1) : (this.rate * (reviewCount + 1) + rate) / Math.min(reviewCount, 1);
+        this.rate = (this.rate * reviewCount + rate) / (reviewCount + 1);
     }
 
     public void addBuyFrom(String buyFrom) {
